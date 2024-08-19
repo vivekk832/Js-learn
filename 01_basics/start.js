7e8f43115200375e48decf41f5637365778292c6
vivekk832 <89959124+vivekk832@users.noreply.github.com>
2024-08-19 21:33:44 +0000

    commit before remote update

--- 01_basics/start.js
+++ 01_basics/start.js
@@ -1,6 +1,10 @@
 const a =4584
 console.log(a)
+<<<<<<< HEAD
 
 console.log("Vivek Kale")
 console.log("Hello World!")
 
+=======
+console.log("Vivek Kale")
+>>>>>>> 605a6f9 (commit before remote update)

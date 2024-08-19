715f2bc9db5538993b88efb091293ac62f78c4b6
vivekk832 <vivekkale9405@gmail.com>
2024-08-20 03:14:41 +0530

    admin

--- 01_basics/start.js
+++ 01_basics/start.js
@@ -1,10 +1,6 @@
 const a =4584
 console.log(a)
-<<<<<<< HEAD
 
 console.log("Vivek Kale")
 console.log("Hello World!")
 
-=======
-console.log("Vivek Kale")
->>>>>>> 605a6f9 (commit before remote update)

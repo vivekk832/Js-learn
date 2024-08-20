1366a8786d4d3f67e6659919deef86a64a7ed449
vivekk832 <89959124+vivekk832@users.noreply.github.com>
2024-08-20 07:43:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"//treat all the Js code as newer version
+//alert(3+3) // we are using nodejs, not browser
+
+// prefer to use mdn documentation (from mozilla )  https://developer.mozilla.org/en-US/docs/Web/JavaScript
+// original documentation https://tc39.es  ecma scripts
+
+let name ="Vivek"
+let age = 18
+let isLoggedIn = false 
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefined => 
+// symbol => unique
+
+
+//object 
+
+console.log(typeof("vivek")); //string
+console.log(typeof(null));  // object 
+console.log(typeof(undefined));//undefined
--- 01_basics/start.js
+++ 01_basics/start.js
@@ -4,3 +4,27 @@ console.log(a)
 console.log("Vivek Kale")
 console.log("Hello World!")
 
+// let const & var
+// const - value cannot change 
+const accountId =  144533
+let accountEmail = "vivekkale123@gmail.com"
+var accountPassword = "12345"
+
+accountCity = "amravati"
+let accountState ;
+
+// accountId = 45315 // not allowed
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and functioal scope
+*/
+
+accountEmail = "vivekkale018@gmail.com"
+accountPassword = 32321321
+accountCity = "nagpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+

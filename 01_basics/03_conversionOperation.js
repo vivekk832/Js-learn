ef59158c78addeeb3c51f6d07b6843424b67c70e
vivekk832 <89959124+vivekk832@users.noreply.github.com>
2024-08-21 03:52:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//for "33abc" o/p:NaN  for "33" o/p:33
+
+// true =>1 false => 0
+
+let isLoggedIn = "Vivek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => flase
+// "" => false
+//"hitesh" => true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+

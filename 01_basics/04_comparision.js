318cf0559f4ca3a0d5c59a72039d861c78699e11
vivekk832 <89959124+vivekk832@users.noreply.github.com>
2024-08-21 04:24:58 +0000

    compairsion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,39 @@
+// console.log(2>1);
+// console.log(2 >=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// try to avoid these type of comparision
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// ===  => along with values ot also check datatype also
+
+console.log("2" === 2);//false
+console.log(2 === 2);//ture
+
+
+
+
+
+
+
+
+
+
+
+
+
+
